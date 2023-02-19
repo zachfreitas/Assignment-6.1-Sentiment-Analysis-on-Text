b9f5464047e199b0c85119539f1fc3a1e57e47d5
Zachariah Freitas <59587801+zachfreitas@users.noreply.github.com>
2023-02-19 11:01:36 -0800

    Save

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+
+# From the "project set-up" experience
+# App Name: zachfreitasusdtextminingclass
+# APP ID: 26534972
+
+api_key = 'XJEXyufx1cejdbUA5L71ZxdEO'
+api_key_secret = 'JH5qM06fXgPmnBLey4oNzHM2GE225x4niOSPeRxDU7MHfYj03p'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAADzklAEAAAAAqL9agBMD6UUO2mXVHbY4FCkI4zw%3Dd99SZoBVtHCwk251dSEsefsAXysfuvmQUKqM0gQos0d2magdmM'
